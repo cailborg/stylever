6419fb45332b7a578af547ac3da34201cdd1ff8c
Cail Borg <Cail.Borg@ofx.com>
2020-08-12 19:38:55 +1000

    update theme

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,5 +1,4 @@
 import * as theme from "./selectors";
-import { ThemeProps, ColorIndex } from "./theme";
 import useTheme from "./useTheme";
-import defaultTheme, { Theme } from "./theme";
+import defaultTheme, { Theme, ThemeProps, ColorIndex } from "./theme";
 export { theme, useTheme, defaultTheme, Theme, ThemeProps, ColorIndex };
